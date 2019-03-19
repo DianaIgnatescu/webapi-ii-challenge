2054aa5c871f9bdef44f3974c228eb08409a0f12
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-19 13:26:38 +0000

    MVP: Complete endpoint for GET request by id

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -42,4 +42,21 @@ router.get('/', (req, res) => {
     });
 });
 
+// === GET BY ID ===
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).json(post);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
 module.exports = router;
