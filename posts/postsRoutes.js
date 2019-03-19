74d45ca8f529c89304b6a1b2f1d973a0af33c00c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-19 13:20:02 +0000

    MVP: Complete endpoint for GET request

--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -30,4 +30,16 @@ router.post('/', (req, res) => {
     });
 });
 
+// === GET ===
+
+router.get('/', (req, res) => {
+  db.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The posts could not be retrieved.' });
+    });
+});
+
 module.exports = router;
