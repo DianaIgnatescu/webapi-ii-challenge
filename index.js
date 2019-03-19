c17069f515a705f2c51e4e9cba8ec537068d328d
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-19 13:59:48 +0000

    MVP: Clean up code

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const postsRoutes = require('./posts/postsRoutes');
 
 const server = express();
 
-// const server = express();
-
 server.use(express.json());
 
 server.use('/api/posts', postsRoutes);
-
-// module.exports = server;
 
 server.listen(5000, () => console.log('Listening on port 5000'));
--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -1,13 +1,11 @@
+/* eslint-disable camelcase */
 const express = require('express');
 
 const router = express.Router();
 const db = require('../data/db');
 
-// === POST ===
-
 router.post('/', (req, res) => {
   const { title, contents } = req.body;
-  // const contents = req.body.contents;
   if (!title || !contents) {
     res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
   }
@@ -25,24 +23,22 @@ router.post('/', (req, res) => {
         updated_at,
       });
     })
+    // eslint-disable-next-line no-unused-vars
     .catch((error) => {
       res.status(500).json({ error: 'There was an error while saving the post to the database.' });
     });
 });
-
-// === GET ===
 
 router.get('/', (req, res) => {
   db.find()
     .then((posts) => {
       res.status(200).json(posts);
     })
+    // eslint-disable-next-line no-unused-vars
     .catch((error) => {
       res.status(500).json({ error: 'The posts could not be retrieved.' });
     });
 });
-
-// === GET BY ID ===
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
@@ -54,12 +50,11 @@ router.get('/:id', (req, res) => {
         res.status(200).json(post);
       }
     })
+    // eslint-disable-next-line no-unused-vars
     .catch((error) => {
       res.status(500).json({ error: 'The post information could not be retrieved.' });
     });
 });
-
-// === DELETE ===
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
@@ -71,18 +66,15 @@ router.delete('/:id', (req, res) => {
         res.status(200).json({ message: `The post with the id ${id} has now been removed from the database.` });
       }
     })
+    // eslint-disable-next-line no-unused-vars
     .catch((error) => {
       res.status(500).json({ errorMessage: 'The post could not be removed.' });
     });
 });
 
-// === PUT ===
-
 router.put('/:id', (req, res) => {
   const { id } = req.params;
   const post = req.body;
-  // const title = req.body.title;
-  // const contents = req.body.contents;
   const { title, contents } = req.body;
   if (!title || !contents) {
     res.status(400).json({ errorMessage: 'Please provide title and contents for the post.' });
@@ -95,6 +87,7 @@ router.put('/:id', (req, res) => {
         res.status(200).json({ post: { id, ...post } });
       }
     })
+    // eslint-disable-next-line no-unused-vars
     .catch((error) => {
       res.status(500).json({ error: 'The post information could not be modified.' });
     });
